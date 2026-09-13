1a7ba0e77b7e4ab462baa54e62a98d2b7ba0ec46
Uni-T17 <mintayzamt17@gmail.com>
2026-09-13 11:34:39 +0700

    Done project set-up

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import routes from "./routes/index.js";
+
